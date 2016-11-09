de7503915fa7c592aa4f53faad2487db3d86b798
patelpb <patelpb@ucla.edu>
2016-11-08 20:56:59 -0800

    Add files via upload

---
+++ HTTP_Server.ino
@@ -0,0 +1,51 @@
+
+    #include <ESP8266WiFi.h>
+     WiFiServer server(80); //Initialize the server on Port 80
+     const short int LED_PIN = 16;//GPIO16
+      void setup() {
+         WiFi.mode(WIFI_AP); //Our ESP8266-12E is an AccessPoint
+         WiFi.softAP("Hello_IoT", "12345678"); // This is the (SSID, password); .
+         server.begin(); // Start the HTTP Server
+         
+         Serial.begin(115200);
+         IPAddress HTTPS_ServerIP= WiFi.softAPIP(); // This is the IP of the Server
+
+         pinMode(LED_PIN, OUTPUT); //GPIO16 is an OUTPUT pin;
+         digitalWrite(LED_PIN, HIGH); //Initial state is OFF
+     }
+     
+     void loop() {
+         WiFiClient client = server.available();
+         if (!client) {
+return; }
+         String request = client.readStringUntil('\r');
+         Serial.println(request);
+         // Handle the Request
+         if (request.indexOf("/ONCE") != -1){
+           digitalWrite(LED_PIN, LOW);
+           delay (1000);
+           digitalWrite(LED_PIN, HIGH);}
+         else if (request.indexOf("/TWICE") != -1){
+           digitalWrite(LED_PIN, LOW);
+           delay (1000);
+           digitalWrite(LED_PIN, HIGH);
+           delay (1000);
+           digitalWrite(LED_PIN, LOW);
+           delay (1000);
+           digitalWrite(LED_PIN, HIGH);
+         }
+         
+         // Prepare the HTML document to respond and add buttons:
+         String s = "HTTP/1.1 200 OK\r\n";
+         s += "Content-Type: text/html\r\n\r\n";
+         s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+         s += "<br><input type=\"button\" name=\"b1\" value=\"LED Once\" onclick=\"location.href='/ONCE'\">";
+         s += "<br><br><br>";
+         s += "<br><input type=\"button\" name=\"b1\" value=\"LED Twice\" onclick=\"location.href='/TWICE'\">";
+         s += "</html>\n";
+         
+         //Serve the HTML document to the browser.
+         client.flush(); //clear previous info in the stream
+         client.print(s); // Send the response to the client
+         delay(1);
+}
